56d8acf79ee97de2ed4a16b057137c3ca50befe3
Muhammad Ariq Srihadi Wibowoputra <muhammadariq13@gmail.com>
2016-11-17 16:49:38 +0700

    Add files via upload

--- FIX/js/grayscale.js
+++ FIX/js/grayscale.js
@@ -34,24 +34,26 @@ $('.navbar-collapse ul li a').click(function() {
 
 $(document).ready(function() {
     
-    /* Every time the window is scrolled ... */
+$(function() {
     $(window).scroll( function(){
     
-        /* Check the location of each desired element */
+       
         $('.fade').each( function(i){
             
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            /* If the object is completely visible in the window, fade it it */
+            /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+            bottom_of_window = bottom_of_window + 200;  
+          
             if( bottom_of_window > bottom_of_object ){
                 
                 $(this).animate({'opacity':'1'},500);
                     
             }
-            
         }); 
     
     });
+});
     
 });
